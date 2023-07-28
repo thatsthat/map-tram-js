7fdd0022db70692c24d7102f29de17b96f0c898d
Aram <aram.anto@gmail.com>
2023-07-28 09:58:41 +0200

    'Plotted the track of T2 tram line'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "leaflet/dist/leaflet.css";
 import "./style.css";
 import L from "leaflet";
+import t2 from "./t2";
 
 document.body.innerHTML = `<div id="map"></div>`;
 
@@ -10,3 +11,5 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution: "© OpenStreetMap",
 }).addTo(map);
+
+L.geoJSON(t2).addTo(map);
